d9c63b0c35a10a4bbec5c58cfbf1f2e142da4ee4
ashishrathore98 <ashrathore98@gmail.com>
2023-07-12 13:04:29 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay3
+{
+    internal class Program
+    {
+        enum Monthsofyear { Jan=1, Feb=2, Mar=3};
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Month: \t" + (int)Monthsofyear.Jan);
+            Console.WriteLine("Second Month: \t" + (int)Monthsofyear.Feb);
+            Console.ReadKey();
+
+            //DataTypes
+
+            //  byte myByte = 12;
+            // sbyte mySByte = 1;
+            // short mySShort = 12;
+            // int myInt = 12;
+            // long myLong = 12;
+            // double myDouble = 12.5;
+            // float myFloat = 12.5F;
+            // decimal myDecimal = 12;
+            // char myChar= 'A';
+            // string myString="Our string";
+            //  DateTime myDateTime = new DateTime(day: 12, month: 12, year: 2022);
+            //bool myBool = false;
+            //try
+            //{
+            //    byte ourByte;
+            //   Console.WriteLine("Enter a number");
+
+            //  ourByte = Convert.ToByte(Console.ReadLine());
+            //      Console.WriteLine("Number is " + ourByte);
+            //  }
+            //  catch (FormatException ex)
+            //  {
+            //    Console.WriteLine("Format Exception " + ex.Message);
+            //}                   
+            //catch (OverflowException ex)
+            //{
+            //   Console.WriteLine("OverFlow Exception " + ex.Message);
+
+            //  }
+            // catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error!!!" + ex.Message);
+
+            //}
+            //finally
+            //{
+            // Console.WriteLine("End of Program!!!");
+            //Console.ReadKey();
+            //}
+
+            
+        
+
+
+            
+        }
+    }
+}
